82e03dccd7f3eb8d724669abb5d56747d11e142c
Tarun Gaba <tarun.gaba7@gmail.com>
2013-04-22 00:55:45 +0530

    Initialized functions.py

--- pydy/webgl/functions.py
+++ pydy/webgl/functions.py
@@ -1,7 +1,13 @@
 #These functions returns the basic functions from the WebGL 
 #in the form of IPython.core.display.Javascript objects.
+try:
+    from IPython.core.display import Javascript
+except:
+    Javascript = None
 
 def InitShaders():
-    pass
+    if not Javascript:
+        raise NotImplementedError('This script requires IPython module to work')
+    
 
 
