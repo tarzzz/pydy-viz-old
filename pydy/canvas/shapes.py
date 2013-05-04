64ffd0ea658a167b3870f7a592e7811b7bf524ac
Tarun Gaba <tarun.gaba7@gmail.com>
2013-05-04 23:56:18 +0530

    Added a new file for shapes. This will include all the basic shapes required for visualization

---
+++ pydy/canvas/shapes.py
@@ -0,0 +1,14 @@
+#This module contains the basic shapes required for the visualization part to work properly
+
+class Sphere(object):
+	"""
+	This class will handle all spherical objects
+	
+	"""
+
+    def __init__(radius,center,*color,*density):
+    
+		"""
+        Initialize the spherical object
+		"""
+         
